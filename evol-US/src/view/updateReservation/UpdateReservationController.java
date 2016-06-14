f43499fc4140123020b6cdaf0e07b82a386d8946
tochyvn <tochlionimy@gmail.com>
2016-06-14 10:06:16 +0200

    Vue ajout/modifier reservation...

---
+++ evol-US/src/view/updateReservation/UpdateReservationController.java
@@ -0,0 +1,15 @@
+package view.updateReservation;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class UpdateReservationController implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+
+}
