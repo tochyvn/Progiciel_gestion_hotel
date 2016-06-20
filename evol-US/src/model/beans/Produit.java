f45e2c6c83b08200053e57faf673ab07ca430617
silnti <tochlionimy@gmail.com>
2016-06-20 10:27:33 +0200

    Commit produit...

--- evol-US/src/model/beans/Produit.java
+++ evol-US/src/model/beans/Produit.java
@@ -1,15 +1,22 @@
 package model.beans;
 
+import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 
 public abstract class Produit {
 
 	private IntegerProperty idProduit;
+	private DoubleProperty tva;
 	
 	
 	public Produit() {
 		super();
+	}
+	
+	public Produit(Double tva) {
+		this.tva = new SimpleDoubleProperty(tva);
 	}
 
 	public IntegerProperty idProduitProperty() {
@@ -23,8 +30,23 @@ public abstract class Produit {
 	public void setIdProduit(int idProduit) {
 		this.idProduit = new SimpleIntegerProperty(idProduit);
 	}
+
+	public final DoubleProperty tvaProperty() {
+		return this.tva;
+	}
 	
 
+	public final double getTva() {
+		return this.tvaProperty().get();
+	}
+	
+
+	public final void setTva(final double tva) {
+		this.tva = new SimpleDoubleProperty(tva);
+	}
+	
+	
+	
 	
 
 }
